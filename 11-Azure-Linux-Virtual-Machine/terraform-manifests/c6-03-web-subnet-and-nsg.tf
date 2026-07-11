7853da19688a7fdba99dacd4c24852e363c34196
tanekemgangdubois <125707819+tanekemgangdubois@users.noreply.github.com>
2026-07-11 08:59:31 -0500

    Update c6-03-web-subnet-and-nsg.tf

--- 11-Azure-Linux-Virtual-Machine/terraform-manifests/c6-03-web-subnet-and-nsg.tf
+++ 11-Azure-Linux-Virtual-Machine/terraform-manifests/c6-03-web-subnet-and-nsg.tf
@@ -46,3 +46,101 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
 }
 
 
+# Resource-1: Create Public IP Address
+resource "azurerm_public_ip" "web_linuxvm_publicip" {
+  name = "${local.resource_name_prefix}-web-linuxvm-publicip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  allocation_method = "Static"
+  sku = "Standard"
+  domain_name_label = "app1-vm-${random_string.myrandom.id}"
+}
+
+# Resource-2: Create Network Interface
+resource "azurerm_network_interface" "web_linuxvm_nic" {
+  name = "${local.resource_name_prefix}-web-linuxvm-nic"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name = "web-linuxvm-ip-1"
+    subnet_id = azurerm_subnet.websubnet.id 
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.web_linuxvm_publicip.id
+  }
+}
+
+locals {
+  webvm_custom_data = <<-CUSTOM_DATA
+#!/bin/bash
+
+# Install Apache
+dnf install -y httpd
+
+# Enable and start Apache
+systemctl enable --now httpd
+
+# Configure firewalld
+systemctl enable --now firewalld
+firewall-cmd --permanent --add-service=http
+firewall-cmd --reload
+
+# Create application directory
+mkdir -p /var/www/html/app1
+
+# Home page
+echo "Welcome to StackSimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+
+# Application pages
+echo "Welcome to StackSimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
+echo "Welcome to StackSimplify - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
+
+cat > /var/www/html/app1/index.html <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <title>StackSimplify</title>
+</head>
+<body style="background-color:rgb(250,210,210);">
+    <h1>Welcome to Stack Simplify - WebVM APP-1</h1>
+    <p>Terraform Demo</p>
+    <p>Application Version: V1</p>
+</body>
+</html>
+EOF
+
+# Azure Instance Metadata
+curl -H "Metadata:true" \
+     --noproxy "*" \
+     "http://169.254.169.254/metadata/instance?api-version=2020-09-01" \
+     -o /var/www/html/app1/metadata.html
+
+CUSTOM_DATA
+}
+
+# Resource: Azure Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "web_linuxvm" {
+  name = "${local.resource_name_prefix}-web-linuxvm"
+  #computer_name = "web-linux-vm" # Hostname of the VM (Optional)
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location 
+  size = "Standard_DS1_v2"
+  admin_username = "azureuser"
+  network_interface_ids = [ azurerm_network_interface.web_linuxvm_nic.id ]
+  admin_ssh_key {
+    username = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+  }
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }  
+  source_image_reference {
+    publisher = "RedHat"
+    offer = "RHEL"
+    sku = "9-lvm-gen2"
+    version = "latest"
+  }  
+  #custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")
+  custom_data = base64encode(local.webvm_custom_data)
+}
